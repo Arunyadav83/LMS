2d8af627e63db02f6e80191f3894861ca4ce4afa
Arunyadav83 <arunyadavbhairi@gmail.com>
2025-01-16 17:21:10 +0530

    push classes.php code with add quize questions

--- admin/function.php
+++ admin/function.php
@@ -1,5 +1,12 @@
 <?php
 function is_admin_logged_in() {
     return isset($_SESSION['admin_logged_in']) && $_SESSION['admin_logged_in'] === true;
+    
 }
+function ensure_directory_exists($directory) {
+    if (!file_exists($directory)) {
+        mkdir($directory, 0777, true);  // Create directory if it doesn't exist
+    }
+}
+
 ?>
